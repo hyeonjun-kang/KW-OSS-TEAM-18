77bf293d57f00d3771633fa51dd9a50949d964f4
ALEXANDER VLAD <grouchyalbatross@gmail.com>
2021-11-07 21:46:43 +0900

    Dice Throwing Function

---
+++ DiceDemo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include "Dice.h"
+using namespace std;
+
+int main() {
+	
+	Dice obj1 = Dice();
+	char inp;
+	cout << "Press [1] to Throw      Press any button to stop" << endl;
+	cin >> inp;
+	while (inp == '1')
+	{
+		obj1.ThrowDice();
+		cin >> inp;
+	}
+
+	return 0;
+}
